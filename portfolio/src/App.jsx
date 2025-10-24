17c23ee51c5bd489b0a8ea2f8a4ce7eab0b201ff
LesMrCode <mhonelesleyeric@gmail.com>
2025-10-24 16:31:59 +0200

    imported components

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,7 @@
-import React from "react";
-
-function App() {
-  return (
-    <div>
-   
-    </div>
-  );
-}
-
+import Header from "./components/Header"; 
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
