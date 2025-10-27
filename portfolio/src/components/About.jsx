2f023cfd5ff813796f8978a5de78adcc949bd294
LesMrCode <mhonelesleyeric@gmail.com>
2025-10-27 18:51:24 +0200

    updated about section

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -2,25 +2,12 @@ import React from 'react'
 
 export default function About() {
 	return (
-    <section id="about" class="about-section">
-      <div class="container col-xxl-8 px-4 py-5">
-        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-          <div class="col-10 col-sm-8 col-lg-6">
-          </div>
-
-		<div class="col-lg-6">
-            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">
-              About Me 
-            </h1>
-            <p class="lead">
-              I’m a passionate software development student at who is a vibrant team player and enjoys problem solving. I’m also excited to apply what I’ve learned in my course to real-world projects and build impactful tech solutions
-            </p>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-              <a href="./Lesley-Eric-CV.pdf" target="_blank" class="btn btn-primary"> Download CV </a>
-            </div>
-          </div>
-        </div>
-        </div> 
+    <section id="about" className='bg-blue flex flex-col items-center justify-center text-center px-6'>
+      <div className='max-w-2xl'>
+        <h1 className='text-4xl text-White mb-4'>About Me</h1>
+        <p className='text-lg font-semibold text-White mb-6'>  I’m a passionate software development student at who is a vibrant team player and enjoys problem solving. I’m also excited to apply what I’ve learned in my course to real-world projects and build impactful tech solutions</p>
+        <a href="./Lesley-Eric-CV.pdf"  class="btn btn-primary"> Download CV </a>
+      </div>
         </section>
 	)
 }
